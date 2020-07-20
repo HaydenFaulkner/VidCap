a885b6d9e61bd1ac9c9f6db3194b4a33dd3e3b2c
HaydenFaulkner <hayyah77@gmail.com>
2020-07-20 16:15:21 +0930

    get rid of the non-essentials

--- datasets/video.py
+++ datasets/video.py
@@ -19,21 +19,12 @@ class VideoDataset(dataset.Dataset):
     setting : str, required.
         A text file describing the dataset, each line per video sample.
         There are three items in each line: (1) video path; (2) video length and (3) video label.
-    train : bool, default True.
-        Whether to load the training or validation set.
-    test_mode : bool, default False.
-        Whether to perform evaluation on the test set.
-        Usually there is three-crop or ten-crop evaluation strategy involved.
-    name_pattern : str, default None.
-        The naming pattern of the decoded video frames.
-        For example, img_00012.jpg.
+    split : str, required.
+        Either of ['train', 'val', 'test']
     video_ext : str, default 'mp4'.
         If video_loader is set to True, please specify the video format accordinly.
     is_color : bool, default True.
         Whether the loaded image is color or grayscale.
-    modality : str, default 'rgb'.
-        Input modalities, we support only rgb video frames for now.
-        Will add support for rgb difference image and optical flow image later.
     num_segments : int, default 1.
         Number of segments to evenly divide the video into clips.
         A useful technique to obtain global video-level information.
@@ -76,12 +67,9 @@ class VideoDataset(dataset.Dataset):
     def __init__(self,
                  root,
                  setting,
-                 train=True,
-                 test_mode=False,
-                 name_pattern='img_%05d.jpg',
+                 split='train',
                  video_ext='mp4',
                  is_color=True,
-                 modality='rgb',
                  num_segments=1,
                  num_crop=1,
                  new_length=1,
@@ -104,10 +92,8 @@ class VideoDataset(dataset.Dataset):
         self.cv2 = try_import_cv2()
         self.root = root
         self.setting = setting
-        self.train = train
-        self.test_mode = test_mode
+        self.split = split
         self.is_color = is_color
-        self.modality = modality
         self.num_segments = num_segments
         self.num_crop = num_crop
         self.new_height = new_height
@@ -119,13 +105,14 @@ class VideoDataset(dataset.Dataset):
         self.target_width = target_width
         self.transform = transform
         self.temporal_jitter = temporal_jitter
-        self.name_pattern = name_pattern
         self.video_ext = video_ext
         self.use_decord = use_decord
         self.slowfast = slowfast
         self.slow_temporal_stride = slow_temporal_stride
         self.fast_temporal_stride = fast_temporal_stride
         self.data_aug = data_aug
+
+        assert split in ['train', 'val', 'test']
 
         if self.slowfast:
             assert slow_temporal_stride % fast_temporal_stride == 0, 'slow_temporal_stride needs to be multiples of slow_temporal_stride, please set it accordinly.'
@@ -163,9 +150,9 @@ class VideoDataset(dataset.Dataset):
             mmcv_vr = self.mmcv.VideoReader(video_name)
             duration = len(mmcv_vr)
 
-        if self.train and not self.test_mode:
+        if self.split == 'train':
             segment_indices, skip_offsets = self._sample_train_indices(duration)
-        elif not self.train and not self.test_mode:
+        elif self.split == 'val':
             segment_indices, skip_offsets = self._sample_val_indices(duration)
         else:
             segment_indices, skip_offsets = self._sample_test_indices(duration)
@@ -208,7 +195,7 @@ class VideoDataset(dataset.Dataset):
             data = split_f.readlines()
             for line in data:
                 line_info = line.split()
-                # line format: video_path, video_duration, video_label
+                # line format: video_path, video_label
                 if len(line_info) < 3:
                     raise(RuntimeError('Video input format is not correct, missing one or more element. %s' % line))
                 clip_path = os.path.join(directory, line_info[0])
@@ -272,59 +259,6 @@ class VideoDataset(dataset.Dataset):
             skip_offsets = np.zeros(
                 self.skip_length // self.new_step, dtype=int)
         return offsets + 1, skip_offsets
-
-    def _image_TSN_cv2_loader(self, directory, duration, indices, skip_offsets):
-        sampled_list = []
-        for seg_ind in indices:
-            offset = int(seg_ind)
-            for i, _ in enumerate(range(0, self.skip_length, self.new_step)):
-                if offset + skip_offsets[i] <= duration:
-                    frame_path = os.path.join(directory, self.name_pattern % (offset + skip_offsets[i]))
-                else:
-                    frame_path = os.path.join(directory, self.name_pattern % (offset))
-                cv_img = self.cv2.imread(frame_path)
-                if cv_img is None:
-                    raise(RuntimeError("Could not load file %s starting at frame %d. Check data path." % (frame_path, offset)))
-                if self.new_width > 0 and self.new_height > 0:
-                    h, w, _ = cv_img.shape
-                    if h != self.new_height or w != self.new_width:
-                        cv_img = self.cv2.resize(cv_img, (self.new_width, self.new_height))
-                cv_img = cv_img[:, :, ::-1]
-                sampled_list.append(cv_img)
-                if offset + self.new_step < duration:
-                    offset += self.new_step
-        return sampled_list
-
-    def _image_slowfast_cv2_loader(self, directory, duration, indices, skip_offsets):
-        sampled_list = []
-        for seg_ind in indices:
-            offset = int(seg_ind)
-            fast_list = []
-            slow_list = []
-            for i, _ in enumerate(range(0, self.skip_length, self.new_step)):
-                if offset + skip_offsets[i] <= duration:
-                    frame_path = os.path.join(directory, self.name_pattern % (offset + skip_offsets[i]))
-                else:
-                    frame_path = os.path.join(directory, self.name_pattern % (offset))
-                if (i + 1) % self.fast_temporal_stride == 0:
-                    cv_img = self.cv2.imread(frame_path)
-                    if cv_img is None:
-                        raise(RuntimeError("Could not load file %s starting at frame %d. Check data path." % (frame_path, offset)))
-                    if self.new_width > 0 and self.new_height > 0:
-                        h, w, _ = cv_img.shape
-                        if h != self.new_height or w != self.new_width:
-                            cv_img = self.cv2.resize(cv_img, (self.new_width, self.new_height))
-                    cv_img = cv_img[:, :, ::-1]
-                    fast_list.append(cv_img)
-
-                    if (i + 1) % self.slow_temporal_stride == 0:
-                        slow_list.append(cv_img)
-
-                if offset + self.new_step < duration:
-                    offset += self.new_step
-            fast_list.extend(slow_list)
-            sampled_list.extend(fast_list)
-        return sampled_list
 
     def _video_TSN_mmcv_loader(self, directory, video_reader, duration, indices, skip_offsets):
         sampled_list = []
@@ -413,5 +347,7 @@ class VideoDataset(dataset.Dataset):
             video_data = video_reader.get_batch(frame_id_list).asnumpy()
             sampled_list = [video_data[vid, :, :, :] for vid, _ in enumerate(frame_id_list)]
         except:
-            raise RuntimeError('Error occured in reading frames {} from video {} of duration {}.'.format(frame_id_list, directory, duration))
+            raise RuntimeError('Error occured in reading frames {} from video {} of duration {}.'.format(frame_id_list,
+                                                                                                         directory,
+                                                                                                         duration))
         return sampled_list
